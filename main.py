4a9539be784576aa48ff24a0f84709d0231f0a4b
korovkinjaroslav <korovkin.jaroslav@gmail.com>
2024-11-26 23:46:58 +0400

    only important

--- main.py
+++ main.py
@@ -1,16 +1,2 @@
 import config
-
-
-for i in range(1000):
-    print(config.token)
-
-name=input()
-
-if name == "Yaroslav" and 2+2==5:
-    print("лох")
-if input('Назовите любое имя\n').lower() in ['Женя', 'Евгений']:
-    print('лох')
-print('аываываы')
-print("bebra")
-print('qweerrsdfsdf')
-print('уга-буга')
+import aiogram
