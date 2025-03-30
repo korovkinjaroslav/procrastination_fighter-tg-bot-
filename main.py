3ad12eb3b5a8f1d9fba52de0cc417ad1fc7f8a74
aqua_minerale <nezabudparol228@gmail.com>
2025-03-30 19:43:23 +0400

    Bot starts

--- main.py
+++ main.py
@@ -4,5 +4,14 @@ import logging
 
 from aiogram import Bot, Dispatcher, executor, types
 
-token = config.token
+API_TOKEN = config.token
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    await message.reply("Я лишь робот. Лишь имитация жизни. Я не сочиню симфонию. Не превращу кусок холста в шедевр искусства.\n я хочу питсу")
 
