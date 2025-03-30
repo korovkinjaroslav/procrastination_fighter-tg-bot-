619b9eb80bdc97ccdabb01c3aeeaeaf25189db68
aqua_minerale <nezabudparol228@gmail.com>
2025-03-30 21:04:57 +0400

    Bot send an answer to everything

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import config
 import logging
 import Task
+import asyncio
 
 from aiogram import Bot, Dispatcher, types
 
@@ -11,11 +12,17 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
 
-greet_message = "Я лишь робот. Лишь имитация жизни. Я не сочиню симфонию. Не превращу кусок холста в шедевр искусства.\n я хочу питсу"
+@dp.message()
+async def greet_message(message: types.Message):
+    await message.answer(
+        text="Я лишь робот. Лишь имитация жизни. Я не сочиню симфонию. Не превращу кусок холста в шедевр искусства.\n я хочу питсу",
+    )
 
 async def main():
     logging.basicConfig(level=logging.INFO)
     await dp.start_polling(bot)
+
+asyncio.run(main())
 
 bebr = Task.CalendarTask('задрилить', '11 9 1488')
 print(bebr.get_info())
